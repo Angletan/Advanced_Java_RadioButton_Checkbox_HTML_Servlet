c790c1bd0a2ea4143a4ae54c4e335fb9e173a621
Tanvi Gupta <118865982+Angletan@users.noreply.github.com>
2022-12-25 06:58:12 +0530

    Add files via upload

---
+++ Insert.java
@@ -0,0 +1,56 @@
+package mca;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Insert
+ */
+@WebServlet("/Insert")
+public class Insert extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Insert() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out=response.getWriter();
+		response.setContentType("text/html");
+		String[] fruit=request.getParameterValues("fruit");
+		String dish=request.getParameter("dish");
+		out.print("<html><body bgcolor=green>");
+		out.print("<ul>");
+		for(String s : fruit)
+		{
+			out.print("<li>" + s + "</li>");
+		}
+		out.print("<h3>Your_Favorite_Dish:<font color=yellow>"+dish+"</font></h3>");
+		out.print("</ul>");
+		out.print("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
